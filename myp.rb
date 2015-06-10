66e1248b5b525c0cf544b34b2a3e91ba4602ea2b
Daniele Parisi <info@danieleparisi.me>
2015-06-10 17:16:55 +0200

    style fix

--- myp.rb
+++ myp.rb
@@ -82,11 +82,11 @@ class Lesson3
 
   def rect x1, y1, x2, y2, t_id
     glBegin GL_QUADS do
-      glNormal3f 0,0,1
-      glVertex3f x1, y1,  0.0
-      glVertex3f x2,  y1,  0.0
-      glVertex3f  x2,  y2,  0.0
-      glVertex3f  x1, y2,  0.0
+      glNormal3f 0, 0, 1
+      glVertex3f x1, y1, 0.0
+      glVertex3f x2, y1, 0.0
+      glVertex3f x2, y2, 0.0
+      glVertex3f x1, y2, 0.0
     end
   end
 
